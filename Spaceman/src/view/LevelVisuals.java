9afe9987f87d0cff3365e07db25eb49a11d241f7
Dylan Fu <dylan.fu@hotmail.co.nz>
2018-04-05 13:13:49 +1200

    Changed Anchorpane to Group

--- Spaceman/src/view/LevelVisuals.java
+++ Spaceman/src/view/LevelVisuals.java
@@ -2,10 +2,14 @@ package view;
 
 import controller.LevelController;//>??
 
+import model.Map;
+import javafx.scene.Group;
 //Scene and layout
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.AnchorPane;
+
+import javafx.scene.shape.*;
 
 //Components
 import javafx.scene.text.*;
@@ -25,6 +29,7 @@ public class LevelVisuals {
 	private double sceneWidth;
 	private double sceneHeight;
 	private Scene scene;
+	private Group pane;
 	
 	public Scene testScene;
 	
@@ -37,9 +42,10 @@ public class LevelVisuals {
 		sceneWidth = 1440.0;
 		sceneHeight = 990.0;//??
 		//this.controller = controller;
-		AnchorPane root = new AnchorPane(); //Layout based on coordinate from edge
+		Group root = new Group(); //Layout based on coordinate from edge
 		root.setStyle("-fx-background-color: null;"); //controls add default css so need to remove
 		addComponents(root); //add nodes to the layout (buttons, text etc)
+		pane = root;
 		
 		Scene level = new Scene(root,sceneWidth,sceneHeight);
 		level.setFill(Color.LIGHTBLUE);
@@ -48,22 +54,21 @@ public class LevelVisuals {
 		scene = level;
 		
 		//test
-		AnchorPane testPane = new AnchorPane();
-		testPane.setStyle("-fx-background-color: null;");
-		Scene test1 = new Scene(testPane, 1440, 980);
-		test1.setFill(Color.LIGHTGREEN);
-		testScene = test1;
+//		AnchorPane testPane = new AnchorPane();
+//		testPane.setStyle("-fx-background-color: null;");
+//		Scene test1 = new Scene(testPane, 1440, 980);
+//		test1.setFill(Color.LIGHTGREEN);
+//		testScene = test1;
 		
 	}
 	public Scene returnScene() {
 		return scene;
 	}
 	
-	private void addComponents(AnchorPane pane) {
+	private void addComponents(Group pane) {
 		
 		//Setup home screen nodes
-		double mapStartY = 100;
-		double mapStartX = 100;
+		
 		
 
 				
@@ -71,6 +76,34 @@ public class LevelVisuals {
 				
 				//Add the nodes to the scene
 				//pane.getChildren().add(title);
+	}
+	
+	public void addTiles(Map mapArray) {
+		
+		double tileWidth = 45;
+		double tileHeight = 45;
+		double mapStartY = (980-tileHeight*21)*0.5; //(WindowH - MapH)/2
+		double mapStartX = (1440 - tileWidth*21)*0.5; //WIndowW - MapW)/2
+		
+		for (int row = 0; row < 21; row++) {
+			for (int col = 0; col < 21; col++) {
+				int currentElement = mapArray.getData(row, col);
+				if (currentElement == 1) {
+					Rectangle wall = new Rectangle(mapStartX+tileWidth*col, mapStartY+tileHeight*row, tileWidth, tileHeight);
+					wall.setFill(Color.INDIANRED); //fill
+					wall.setStroke(Color.INDIANRED);//storke
+					pane.getChildren().add(wall);
+				} else if (currentElement == 2) {
+					Circle pellet = new Circle(mapStartX+tileWidth*col+tileWidth*0.5, mapStartY+tileHeight*row+tileHeight*0.5, tileWidth*0.125);
+					pellet.setFill(Color.LEMONCHIFFON);
+					pane.getChildren().add(pellet);
+				} else if (currentElement == 3) {
+					Circle powerup = new Circle(mapStartX+tileWidth*col+tileWidth*0.5, mapStartY+tileHeight*row+tileHeight*0.5, tileWidth*0.35);
+					powerup.setFill(Color.CRIMSON);
+					pane.getChildren().add(powerup);
+				}
+			}
+		}
 	}
 
 }
